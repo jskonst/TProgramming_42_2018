47093d1440cfe0124589c527ff520d553a8fcb7c
K-Voland <kosta_v@mail.ru>
2018-10-16 13:40:35 +0400

    lab_2

---
+++ Lab_2/llab2.cs
@@ -0,0 +1,45 @@
+using System;
+ 
+namespace CourseApp4
+{
+    class Program4
+    {
+       
+        static void Main(string[] args)
+        {
+
+         double xn = 0.7;
+         double xk = 2.2;
+         double dx = 0.3;
+         double[] xx = {0.25, 0.36, 0.56, 0.94, 1.28};
+         
+         Console.WriteLine ("Задание А");
+         
+         for( double x=xn; x<xk; x+=dx)
+            {
+               Console.WriteLine (Fan(x));
+                
+            }
+
+          Console.WriteLine ("Задание B");
+
+         foreach( double x in xx)
+            {
+                Console.WriteLine (Fan(x));
+            }
+
+        }   
+
+        static double Fan(double x)
+        { 
+         double a = 1.2;
+         double b = 0.48;
+         double y;
+
+         y = (b*b*b + Math.Sin(a*x)*Math.Sin(a*x)) / (Math.Acos(x*b*x) + Math.Exp(-x/2));
+         return y;
+        }
+       
+     }
+
+}   
