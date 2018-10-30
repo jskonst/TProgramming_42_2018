e42b7ebc53d771a2108f9ab2082bbdccd6451366
K-Voland <kosta_v@mail.ru>
2018-10-30 13:45:09 +0400

    lab_2_2

---
+++ CourseApp.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+using CourseApp;
+
+namespace CourseApp.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+        fun()
+        }
+    }
+}
--- CourseApp/Program.cs
+++ CourseApp/Program.cs
@@ -1,12 +1,45 @@
 ﻿using System;
-
-namespace CourseApp
+ 
+namespace CourseApp4
 {
-    class Program
+    class Program2
     {
+        static double a = 1.2;
+        static double b = 0.48;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Волошин Константин");
+
+         double xn = 0.7;
+         double xk = 2.2;
+         double dx = 0.3;
+         double[] xx = {0.25, 0.36, 0.56, 0.94, 1.28};
+         
+         Console.WriteLine ("Задание А");
+         
+         for( double x=xn; x<xk; x+=dx)
+            {
+               Console.WriteLine (Fan(x));
+                
+            }
+
+          Console.WriteLine ("Задание B");
+
+         foreach( double x in xx)
+            {
+                Console.WriteLine (Fan(x));
+            }
+
+        }   
+
+        public static double Fan(double x)
+        { 
+
+        double y = (b*b*b + Math.Sin(a*x)*Math.Sin(a*x)) / (Math.Acos(x*b*x) + Math.Exp(-x/2));
+         return y;
         }
-    }
-}
+       
+     }
+
+}   
+
