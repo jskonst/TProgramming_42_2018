861174138c1363816028520b835b60b58c5eaf12
Ihateagit <43777289+Dezimeity@users.noreply.github.com>
2018-11-11 23:45:56 +0300

    Vera afanaseva (#24)
    * Fix and make part of test

---
+++ ConsoleApp.Tests/UnitTest1.cs
@@ -0,0 +1,30 @@
+using System;
+using Xunit;
+using ConsoleApp3;
+
+namespace ConsoleApp3.Tests
+{
+    public class UnitTest1
+    {
+        double a = 1.35;
+        double b = 0.98;
+        double xn = 1.14;
+        double xk = 4.24;
+        double dx = 0.62;
+        double[] z = new double[] { 0.35, 1.28, 3.51, 5.21, 4.16 };
+
+        [Fact]
+        public void Test1()
+        {
+            var result = Program.formula(0.0, 1.35, 0.98);
+            Assert.Equal(0, result);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var result = Program.formula(1.14, 0.0, 0.98);
+            Assert.Equal(308.817, result,3);
+        }
+    }
+}
