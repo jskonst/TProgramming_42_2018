4d34fd5ae8b99d36052b9c2b0271740d8e34190b
Ihateagit <43777289+Dezimeity@users.noreply.github.com>
2018-11-13 13:55:45 +0400

    Vera afanaseva (#38)
    * Very hard first work
    
    * Popravila
    
    * very hard test
    
    * Popravila-2
    
    * Fix and make part of test
    
    * Fix1
    
    * Fix2
    
    * Fix3

---
+++ CourseApp/CourseApp/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace CourseApp
+{
+    public class Program
+    {
+        
+        public static double formula(double x, double a, double b)
+        {
+
+            return(Math.Pow(a * x + b, (1 / 3)) / (Math.Pow(Math.Log10(x), 2)));
+                  
+        }
+
+        static void Main()
+        {
+            Console.WriteLine("2 вариант");
+            Console.WriteLine();
+            Console.WriteLine("Задание А");
+            double a = 1.35;
+            double b = 0.98;
+            double xn = 1.14;
+            double xk = 4.24;
+            double dx = 0.62;
+
+
+            for (double x=xn; x <= xk; x = x + dx)
+            {
+                Console.WriteLine($" x={x} y={Math.Round(formula(x, a, b), 3)}");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Задание B");
+            double[] z = new double[] { 0.35, 1.28, 3.51, 5.21, 4.16 };
+            foreach (double element in z)
+            {
+                Console.WriteLine($" X={element} y={Math.Round(formula(element, a, b), 3)} ");
+            }
+
+
+            Console.ReadKey();
+        }
+
+    }
+}
